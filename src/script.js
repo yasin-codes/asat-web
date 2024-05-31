000bc903bc9478e3996e57cb0c16d65d2dd0e923
Yasin <ahmadyasin.abdr@gmail.com>
2024-05-31 21:23:20 +0700

    added modal

--- src/script.js
+++ src/script.js
@@ -5,3 +5,27 @@ hamburger.addEventListener("click", () => {
   console.log(hamburgerList);
   hamburgerList.classList.toggle("hidden");
 });
+
+document.getElementById("open-modal").addEventListener("click", function () {
+  document.getElementById("modal").classList.remove("hidden");
+});
+
+document.getElementById("close-modal").addEventListener("click", function () {
+  document.getElementById("modal").classList.add("hidden");
+});
+
+document.addEventListener("click", function (event) {
+  const modal = document.getElementById("modal");
+  if (!modal.contains(event.target) && !document.getElementById("open-modal").contains(event.target)) {
+    modal.classList.add("hidden");
+  }
+});
+
+let navToggle = document.querySelector(".nav-toggle");
+let bars = document.querySelectorAll(".bar");
+
+function toggleHamburger(e) {
+  bars.forEach((bar) => bar.classList.toggle("x"));
+}
+
+navToggle.addEventListener("click", toggleHamburger);
